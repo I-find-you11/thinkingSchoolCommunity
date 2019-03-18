2f7fe2fb133eef7bab94278f4f371c0424ba6417
11 <841850130@qq.com>
2019-03-18 09:52:38 +0800

    修改缩进

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        九段前端社区
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				九段前端社区
+			</div>
+		);
+	}
 }
 
 export default App;
